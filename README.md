3b1dd8b050f6e278a9a9ab5d27ca6c81eda0f497
Niklan <niklanrus@gmail.com>
2017-05-10 17:46:34 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Contact Tools
+
+Module for Drupal 8 which provide some helpers to work with contact module forms.
+
+## Call service
+
+All provided tools is now accessible via service `contact_tools`.
+
+```php
+$contact_tools = \Drupal::service('contact_tools');
+```
+
+After that you can call all methods.
+
+### createModalLink($link_title, $contact_form, $url_options = [], $modal_options = [])
+
+Return renderable array type 'link' which will open contact form in Modal window.
+
+By default `$link_options` provide those settings:
+
+```php
+$link_options_defaults = [
+  'attributes' => [
+    'class' => ['use-ajax'],
+    'data-dialog-type' => 'modal',
+    'data-dialog-options' => Json::encode([
+      'width' => 'auto',
+    ]),
+  ],
+];
+```
+
+You can modify it by provided additional settings and\or replace default ones.
+
+#### Example 1
+
+```php
+$contact_tools = \Drupal::service('contact_tools');
+return $contact_tools::createModalLink('Call me', 'callback');
+```
+
+#### Example 2
+
+Pass some values to the form via query.
+
+```php
+$contact_tools = \Drupal::service('contact_tools');
+return $contact_tools::createModalLink('Call me', 'callback', ['query' => ['product' => $node->id()]]);
+```
---
+++ src/ContactTools.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\contact_tools;
+
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Url;
+
+/**
+ * Main class for all snippets and helpers.
+ */
+class ContactTools {
+
+  /**
+   * @param       $link_title
+   *   The title of the link.
+   * @param       $contact_form
+   *   The machine name of contact form needed to be loaded in modal.
+   * @param array $url_options
+   *   (optional) An array of options. Mainly used for pass GET parameters.
+   *   See Url::fromUri() for details.
+   * @param array $link_options
+   *   (optional) An array of options. Here must be passed all settings for
+   *   modal window. See Url::fromUri() for details.
+   *
+   * @return array
+   *   Renderable array with link.
+   */
+  public static function createModalLink($link_title, $contact_form, $url_options = [], $link_options = []) {
+    $link_options_defaults = [
+      'attributes' => [
+        'class' => ['use-ajax'],
+        'data-dialog-type' => 'modal',
+        'data-dialog-options' => Json::encode([
+          'width' => 'auto',
+        ]),
+      ],
+    ];
+
+    $link_options = array_merge($link_options_defaults, $link_options);
+
+    return [
+      '#type' => 'link',
+      '#title' => $link_title,
+      '#url' => Url::fromRoute('entity.contact_form.canonical', ['contact_form' => $contact_form], $url_options),
+      '#options' => $link_options,
+      '#attached' => ['library' => ['core/drupal.dialog.ajax']],
+    ];
+  }
+
+}
